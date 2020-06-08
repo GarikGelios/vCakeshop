47555390ad8968ba900c8c632bb258f310d60056
GarikGelios <garikgelios@gmail.com>
2020-06-08 22:56:41 +0300

    add to header logo, phone, button

--- src/components/v-header.vue
+++ src/components/v-header.vue
@@ -1,9 +1,26 @@
 <template>
   <header class="v-header">
+    <picture>
+      <source
+        srcset="@/assets/logo/logo.svg"
+        media="(min-width: 600px)"
+        type="image/svg+xml"
+      />
+      <img src="@/assets/logo/logo.png" alt="logo" width="50" height="50" />
+    </picture>
     <nav class="v-header_nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
+    <div class="v-header_contacts">
+      <div class="v-header_contacts_phone">
+        <a :href='"tel:" + phone'>{{ phone }}</a>
+        <p>Call to us</p>
+      </div>
+      <button class="v-header_contacts_button">
+        Make order
+      </button>
+    </div>
   </header>
 </template>
 
@@ -11,18 +28,39 @@
 export default {
   name: 'v-header',
   data () {
-    return {}
+    return {
+      phone: '+0123456789'
+    }
   }
 }
 </script>
 
 <style lang="scss">
 .v-header {
-  padding: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1200px;
+  margin: auto;
+  padding: 30px 0;
+  &_contacts {
+    display: flex;
+    &_phone{
+      margin: 0 10px;
+      p {
+        margin: 0;
+      }
+    }
+    &_button{
+      background: transparent;
+      border-radius: 50px;
+    }
+  }
 
   a {
     font-weight: bold;
     color: #2c3e50;
+    text-decoration: none;
 
     &.router-link-exact-active {
       color: #42b983;
