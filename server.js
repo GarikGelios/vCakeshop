93d70341fd874c72d84c8959f8187504ffb71a63
GarikGelios <garikgelios@gmail.com>
2020-08-06 20:59:44 +0300

    edit email

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.post('/', (req, res) => {
   const message = {
     from: 'Sweet Home — Cake Shop <order@sweethome-cyprus.com>', // sender address
     to: `order@sweethome-cyprus.com`, // list of receivers
-    cc: ['lusineadamyan07@gmail.com'],
+    // cc: ['lusineadamyan07@gmail.com'],
     // bcc: ['garikgelios@gmail.com'],
     subject: 'Тема сообщения', // Subject line
     html: content(req.body)
